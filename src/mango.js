e231e20b00172bf450c938a9664a51a030467537
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-11-09 10:06:58 +0100

    Settig up tests

--- src/mango.js
+++ src/mango.js
@@ -1,4 +1,4 @@
-function mango() {
+function mango(quantaty, price) {
   return 'mango';
 }
 
--- src/mango.test.js
+++ src/mango.test.js
@@ -1,7 +1,11 @@
 const mango = require('./mango');
 
 describe('mango', () => {
-  it('should return mango', () => {
-    expect(mango()).toBe('mango');
+  it('3,3 should be 6', () => {
+    expect(mango(3,3)).toBe(6);
   });
+  it('9,5 should be 30', () => {
+    expect(mango(9,5)).toBe(30);
+  });
+
 });
